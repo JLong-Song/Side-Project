a184cdc94097232ff2326808d3b6eaf0aa0b7a8a
JLong-Song <iloveyou870116@gmail.com>
2021-03-06 13:08:33 +0800

    2021/3/6

---
+++ test/LINE_notify.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_GET['message'])){
+        $headers = array(
+            'Content-Type: multipart/form-data',
+            'Authorization: Bearer p8SeaSfBaL2cPSJHjpCgmy7Hp6gEl0xJFoAGsAWDZtk'
+        );
+        $message = array(
+            'message' => $_GET['message']
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+?>
