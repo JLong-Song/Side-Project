7956eaa3041701c47a6dbd2474a87a34aff86175
JLong-Song <iloveyou870116@gmail.com>
2021-03-10 13:04:04 +0800

    2021/03/10

--- Website/Flask/main.py
+++ Website/Flask/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from crc_16 import *
 app = Flask(__name__)
 app.debug = True
 
@@ -13,6 +13,7 @@ def index():
 def login():
     account = request.values.get('Account')
     password = request.values.get('Password')
+    return f'<p>{crc_16(str(account))}</p>'
 
 
 if __name__ == '__main__':
---
+++ Website/Flask/security.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def crc_16(str):
+    list = []
+    list.extend(ord(c) for c in str)
+    register = 0xFFFF
+    for c in list:
+        register ^= c
+        for _ in range(8):
+            LSB = register & 1
+            register = register >> 1
+            if LSB == 1:
+                register ^= 0xA001
+    list.append(register & 0x00FF)
+    list.append((register & 0xFF00) >> 8)
+    return (str,list)
+
+def format():
+    pass
