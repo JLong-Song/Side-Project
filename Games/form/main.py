e65af9c94cce33330fa0c3c11d04b008414af4f6
JLong-Song <iloveyou870116@gmail.com>
2021-03-11 17:33:08 +0800

    2021/03/11

--- Games/form/main.py
+++ Games/form/main.py
@@ -2,12 +2,14 @@ import tkinter as tk
 
 
 def open_setting():
-    pass
+    setting_window = tk.Toplevel(window)
+    setting_window.geometry("240x160+0+0")
+    setting_window.mainloop()
 
 
 window = tk.Tk()
 window.title("Form")
-window.geometry("480x320")
+window.geometry("480x320+0+0")
 
 setting = tk.Button(window, text="Setting", fg="red",
                     font=("Times New Roman", 12), command=open_setting)
