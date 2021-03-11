a650e11be238443240b9e8c5ba20fb0310e8b059
JLong-Song <iloveyou870116@gmail.com>
2021-03-11 17:29:26 +0800

    2021/03/11

--- Games/form/main.py
+++ Games/form/main.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 
+
+def open_setting():
+    pass
+
+
 window = tk.Tk()
 window.title("Form")
 window.geometry("480x320")
+
+setting = tk.Button(window, text="Setting", fg="red",
+                    font=("Times New Roman", 12), command=open_setting)
+setting.pack()
+
+
 window.mainloop()
