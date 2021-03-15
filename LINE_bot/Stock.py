e8b794540cfadbac26816a87b7bb4c5dafdfe3cc
JLong-Song <iloveyou870116@gmail.com>
2021-03-15 11:23:11 +0800

    2021/03/15

--- LINE_bot/Stock.py
+++ LINE_bot/Stock.py
@@ -11,8 +11,10 @@ def stock_2330():
         high_price = stock['realtime']['high']
         low_price = stock['realtime']['low']
         message = f"\n{name}({code}):\n[High]{high_price}\n[Low]{low_price}"
-        response = requests.get("http://127.0.0.1/test/LINE_notify.php?message=" + message)
-        print(f"Status : {response.json()['message']}")
+    else:
+        message = stock['rtmessage']
+    response = requests.get("http://127.0.0.1/test/LINE_notify.php?message=" + message)
+    print(f"Status : {response.json()['message']}")
     Timer(3, stock_2330).start()
 
 
