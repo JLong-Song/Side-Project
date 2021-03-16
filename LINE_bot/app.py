e56194e37801ba015b213099aed80dbfca3f7ced
JLong-Song <iloveyou870116@gmail.com>
2021-03-16 09:27:13 +0800

    2021/03/16

--- LINE_bot/app.py
+++ LINE_bot/app.py
@@ -2,11 +2,24 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import twstock
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('9uxeW5d1r8G2mmtQg7z+U0AsSzaJppLQtI+mbvrudH6ynp+KRKwvwZGnUd81O90UvwdG0IwKj4+54X78iRV07XNEXB/qXlcXVpW2hxAZUac9e71z3wMUFwNIW2Er3CUUUQzXwWjrdzsOJkGBijSPQgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('0b6caaf9fea04049ba06471aec26fd1f')
+
+def stock(num):
+    stock = twstock.realtime.get(num)
+    if stock['success']:
+        name = stock['info']['name']
+        code = stock['info']['code']
+        high_price = stock['realtime']['high']
+        low_price = stock['realtime']['low']
+        message = f"\n{name}({code}):\n[High]{high_price}\n[Low]{low_price}"
+    else:
+        message = stock['rtmessage']
+    return message
 
 @app.route('/callback', methods=['POST'])
 def callback():
@@ -24,7 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextMessage(text='test')
+    message = TextMessage(text=stock(event))
     line_bot_api.reply_message(event.reply_token, message)
 
 if __name__ == '__main__':
